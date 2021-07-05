f15c4db30d3294cbb3f0f3f83f57cba24aae2432
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-04 20:03:33 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(int)
+    if int <= 1
+        false
+    else
+        sqrt =
+        (int**0.5).truncate
+        array = (2..sqrt).to_a
+        !array.any? do |num|
+        int % num == 0
+        end
+    end
+
+end
